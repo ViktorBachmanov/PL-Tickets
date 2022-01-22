096319da1803be0fa2599072ff22c3e0d0e29e6d
Vikt0r <home@home.ru>
2022-01-22 21:12:56 +0300

    Initialize light/dark theme depending in system preference

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,33 +2,64 @@
 /** @jsxImportSource @emotion/react */
 
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { css } from '@emotion/react';
 
-import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 
 import SideBar from './SideBar';
 import AppBarTickets from '../features/appbar/AppBarTickets';
 import Login from './Login';
+import { setLightStatus as setLightStatusAction } from '../features/theme/themeSlice';
 import createMainTheme from '../features/theme/mainTheme';
 import { LightStatus } from '../features/theme/types';
 
-export default function Layout() {
-  const lightMode: LightStatus = useSelector((state: RootState) => state.theme.lightStatus);
 
-  const loginStatus = useSelector((state: RootState) => state.user.loginStatus);
+function mapStateToProps(state: RootState) {
+  return {
+    loginStatus: state.user.loginStatus,
+    lightMode: state.theme.lightStatus,  
+  };
+}
+
+const mapDispatchToProps = {
+  setLightStatus: setLightStatusAction,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+function Layout(props: PropsFromRedux) {
+  const { loginStatus, lightMode, setLightStatus } = props;
+
+  //console.log('Layout');
+
+  const isPrefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  //console.log('isPrefersDarkMode: ', isPrefersDarkMode);
+  const systemLightMode = isPrefersDarkMode ? LightStatus.DARK : LightStatus.LIGHT;
+  React.useEffect(() => {
+    setLightStatus(systemLightMode);
+  }, [systemLightMode, setLightStatus]); 
+
+  const mainTheme = React.useMemo(
+    () => createMainTheme(lightMode),
+    [lightMode]
+  );
+
+
   if (!loginStatus) {
     return <Login />;
   }
 
-  const mainTheme = createMainTheme(lightMode);
 
   return (
     <Box
@@ -60,3 +91,6 @@ export default function Layout() {
     </Box>
   );
 }
+
+
+export default connector(Layout);
